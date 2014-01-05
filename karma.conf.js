2fef0144e2bf1de598db56ad0a1f7f445043a889
Felix Zapata <fzberlinches@gmail.com>
2014-01-05 17:52:03 +0100

    feat(chai): añade chai para hacer los tests

--- karma.conf.js
+++ karma.conf.js
@@ -17,10 +17,11 @@ module.exports = function(config) {
             'app/bower_components/angular-cookies/angular-cookies.js',
             'app/bower_components/angular-sanitize/angular-sanitize.js',
             'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/chai/chai.js',
             'app/scripts/*.js',
             'app/scripts/**/*.js',
             'test/spec/**/*.js',
-            'app/views/*.html',
+            'app/views/*.html'
         ],
 
         // generate js files from html templates
